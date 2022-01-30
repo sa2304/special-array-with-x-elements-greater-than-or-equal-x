9717817c3ab7ed26d0755943a474856f0b2d9e3e
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-30 16:34:27 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -7,7 +8,15 @@ using namespace std;
 class Solution {
  public:
   int specialArray(vector<int> &nums) {
-    // FIXME
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); ++i) {
+      int x = nums.size() - i;
+      auto iter = lower_bound(nums.begin(), nums.end(), x);
+      if (distance(iter, nums.end()) == x) {
+        return x;
+      }
+    }
+
     return -1;
   }
 };
@@ -25,6 +34,10 @@ void TestSpecialArray() {
   {
     vector<int> nums{0, 4, 3, 0, 4};
     assert(3 == s.specialArray(nums));
+  }
+  {
+    vector<int> nums{3, 6, 7, 7, 0};
+    assert(-1 == s.specialArray(nums));
   }
 }
 
